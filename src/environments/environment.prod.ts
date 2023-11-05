79f80163b6553a29f897890be26de31bb3bedb5a
Ganeshgit123 <liai.ganeshkumar@gmail.com>
2023-11-05 19:21:16 +0530

    market_change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   baseUrl: 'https://api1.webdev.juman-bm.com'
-  // baseUrl: 'http://api-test.juman.sa'
+  // baseUrl: 'https://api-test.juman.sa'
 };
