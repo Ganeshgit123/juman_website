eb296bb747fdfb674600b72d4655c66de0aeca81
Ganeshgit123 <liai.ganeshkumar@gmail.com>
2024-02-29 08:05:44 +0530

    baseUrl change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
   // baseUrl: 'https://api1.webdev.juman-bm.com'
-  baseUrl: 'https://api-test.juman.sa'
+  // baseUrl: 'https://api-test.juman.sa'
+  baseUrl: 'https://api.juman.sa'
 };
